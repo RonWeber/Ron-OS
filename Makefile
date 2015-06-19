39882fc84a23be4e38b56283dcc8001f418184b0
Bratmon <Ronnieweber@gmail.com>
2015-06-19 19:30:43 -0400

    Split into multiple files.  Changed makefile to accomidate this.

--- Makefile
+++ Makefile
@@ -1,21 +1,21 @@
 
-SOURCES=start.o main.o
+SOURCES=start.o main.o vga.o
 
 CFLAGS=-std=gnu99 -ffreestanding -O2 -Wall -Wextra
 CC = i686-elf-gcc
 LDFLAGS=-Tlink.ld
 ASFLAGS=-felf32
 
-all: $(SOURCES) link 
+all: ros.bin
 
 clean:
 	-rm *.o ros.bin
 
-link:
+ros.bin: $(SOURCES) link.ld
 	i686-elf-gcc $(LDFLAGS) -o ros.bin -ffreestanding -O2 -nostdlib $(SOURCES) -lgcc
 
-start.o: start.asm
+%.o: %.asm
 	nasm $(ASFLAGS) $<
 
-main.o: main.c
+%.o: %.c
 	$(CC) -c $< -o $@ $(CFLAGS)
---
+++ vga.c
@@ -0,0 +1,75 @@
+#include "vga.h"
+#include <stdint.h>
+#include <stddef.h>
+
+size_t strlen(const char* str)
+{
+	size_t nullCharPos = 0;
+	while (str[nullCharPos] != 0)
+		nullCharPos++;
+	return nullCharPos;
+}
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+
+uint16_t make_vgaentry(char c, uint8_t color) {
+	uint16_t c16 = c;
+	uint16_t color16 = color;
+	return c16 | color16 << 8;
+}
+
+size_t terminal_row, terminal_col;
+uint16_t* terminal_buffer;
+
+void terminal_initialize() {
+	terminal_row = 0;
+	terminal_col = 0;
+	uint8_t color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000; //I like the zen of memory address casts.  Why is this at 0xB8000?  Because it is.
+	for (size_t row = 0; row < VGA_HEIGHT; row++)
+	{
+	    for(size_t col = 0; col < VGA_WIDTH; col++)
+	    {
+		const size_t index = row * VGA_WIDTH + col;
+		terminal_buffer[index] = make_vgaentry(' ', color);
+	    }
+	}
+      
+}
+
+void terminal_changeEntryTo(char c, uint8_t color, size_t x, size_t y)
+{
+    const size_t index = y * VGA_WIDTH + x;
+    terminal_buffer[index] = make_vgaentry(c, color);
+}
+
+void terminal_putchar(char c, uint8_t color)
+{
+    if (c == '\n') //Newlines have to be supported at some point.
+    {
+	if (++terminal_row == VGA_HEIGHT)
+	{
+	    terminal_row = 0;
+	}
+    }
+    else
+    {
+	terminal_changeEntryTo(c, color, terminal_col, terminal_row);
+	if (++terminal_col == VGA_WIDTH)
+	{
+	    terminal_col = 0;
+	    if (++terminal_row == VGA_HEIGHT)
+	    {
+		terminal_row = 0;
+	    }
+	}
+    }
+}
+
+void terminal_print(const char* data, uint8_t color)
+{
+    size_t datalen = strlen(data);
+    for (size_t i = 0; i < datalen; i++)
+	terminal_putchar(data[i], color);
+}
